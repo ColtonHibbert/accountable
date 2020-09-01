4910aa5a7ce5736f7a54ec1a2808b622d49668df
ColtonHibbert <39970072+ColtonHibbert@users.noreply.github.com>
2020-09-01 12:00:50 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Goal from './Goal.js';
 import List from './List.js';
 import GoalComment from './GoalComment.js';
 
 function App() {
+  const [count, setCount] = useState(0);
   
   return (
     <div className="App">
@@ -16,6 +17,10 @@ function App() {
       <div onClick={() => fetch('http://localhost:3001/hi')}>
       get
       </div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
   );
 }
